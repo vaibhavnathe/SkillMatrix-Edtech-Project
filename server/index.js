9a7fe5d77418739ed0cc1c4041f7da431cc871dd
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-04-25 22:02:52 +0530

    SkillMatrix Project Uplaoding..

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+
+const app = express();
+
+const userRoutes = require("./routes/User");
+const paymentRoutes = require("./routes/Payments");
+const profileRoutes = require("./routes/Profile");
+const CourseRoutes = require("./routes/Course");
+
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { cloudnairyconnect } = require("./config/cloudinary");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+database.connect();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    maxAge: 14400,
+  })
+);
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization");
+  next();
+});
+
+
+
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
+cloudnairyconnect();
+
+app.use("/api/v1/auth", userRoutes);
+
+app.use("/api/v1/payment", paymentRoutes);
+
+app.use("/api/v1/profile", profileRoutes);
+
+app.use("/api/v1/course", CourseRoutes);
+
+app.use("/api/v1/contact", require("./routes/ContactUs"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
